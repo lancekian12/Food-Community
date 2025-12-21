459d8ee4881389d349191ca0ab73aeb1a35d018a
Lance Kian Flores <lancekian12@gmail.com>
2025-12-21 15:05:47 +0800

    outputting the meals

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 const MealPage = () => {
-  console.log('Meal Page')
   return (
-    <div>
-      <h1>Meals Page</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicous meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favorite Recipe </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
