6128b3e46c8a423c673284e73c6d9aedccc34afe
Lance Kian Flores <lancekian12@gmail.com>
2025-12-21 15:27:02 +0800

    modify link and make it client directive

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import React from "react";
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
-  console.log("Executing")
   return (
     <>
       <MainHeaderBackground />
@@ -19,10 +18,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href='/meals'>Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href='/community'>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
