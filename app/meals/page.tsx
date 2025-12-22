eeeae76fb2de5be60bdbf08de34b1e90c4413a6b
Lance Kian Flores <lancekian12@gmail.com>
2025-12-22 23:05:23 +0800

    add suspense in fetching meals

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meal";
-const MealPage = async () => {
+
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+const MealPage = async () => {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +25,11 @@ const MealPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals....</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
