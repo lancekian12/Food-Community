a593ca01afd2779d349b395324376d8fa3669997
Lance Kian Flores <lancekian12@gmail.com>
2025-12-26 22:09:00 +0800

    separate server action

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,19 +1,11 @@
+'use client'
+
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/action";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
 
-    const meal = {
-      title: formData.get("title"),
-        summary: formData.get("summary"),
-        instructions: formData.get('instructions'),
-        image: formData.get('image'),
-        creator_email: formData.get('email')
-      };
-      console.log(meal)
-  }
   return (
     <>
       <header className={classes.header}>
@@ -23,39 +15,39 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
-          <div className={classes.row}>
-            <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
-            </p>
-            <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
-            </p>
-          </div>
-          <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
-          </p>
-          <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
-          </p>
-          <p>
-            <label htmlFor="instructions">Instructions</label>
-            <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
-              required
-            ></textarea>
-          </p>
-          <ImagePicker label="Your Image" name="image" />
-          <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
-          </p>
-        </form>
+<form className={classes.form} action={shareMeal}>
+  <div className={classes.row}>
+    <p>
+      <label htmlFor="name">Your name</label>
+      <input type="text" id="name" name="name" required />
+    </p>
+    <p>
+      <label htmlFor="email">Your email</label>
+      <input type="email" id="email" name="email" required />
+    </p>
+  </div>
+  <p>
+    <label htmlFor="title">Title</label>
+    <input type="text" id="title" name="title" required />
+  </p>
+  <p>
+    <label htmlFor="summary">Short Summary</label>
+    <input type="text" id="summary" name="summary" required />
+  </p>
+  <p>
+    <label htmlFor="instructions">Instructions</label>
+    <textarea
+      id="instructions"
+      name="instructions"
+      rows="10"
+      required
+    ></textarea>
+  </p>
+  <ImagePicker label="Your Image" name="image" />
+  <p className={classes.actions}>
+    <button type="submit">Share Meal</button>
+  </p>
+</form>
       </main>
     </>
   );
