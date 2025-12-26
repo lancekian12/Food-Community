329c8e6201b30980de99921a90b9a8878039f4c5
Lance Kian Flores <lancekian12@gmail.com>
2025-12-26 22:51:06 +0800

    adding server side input validation

--- lib/action.ts
+++ lib/action.ts
@@ -3,6 +3,9 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meal";
 
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get("title"),
@@ -12,6 +15,17 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    !meal.creator_email.include("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid Input");
+  }
   await saveMeal(meal);
 
   redirect("/meals");
